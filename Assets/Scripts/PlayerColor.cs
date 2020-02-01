a8a52dcc9154c416376ace1cc17f69fe278dd0a1
Gabriel Massia Mihalyi <gmassia95@gmail.com>
2020-02-01 13:29:40 -0300

    Finished level desing

---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColor : MonoBehaviour
+{
+    public Color playerColor;
+    public MeshRenderer bodyRenderer;
+    public ParticleSystem particles;
+
+    void Start()
+    {
+        bodyRenderer.material.color = playerColor;
+        if (particles != null)
+        {
+            ParticleSystem.MainModule particleSettings = particles.main;
+            particleSettings.startColor = new ParticleSystem.MinMaxGradient(playerColor);
+        }
+    }
+}
---
+++ Assets/Scripts/RepairPlatform.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairPlatform : MonoBehaviour
+{
+    public GameObject belongsTo;
+    public GameObject linkedSpaceship;
+}
